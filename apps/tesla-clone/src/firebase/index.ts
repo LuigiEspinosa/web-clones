ec8ed8e56ec9fa3e9c1116cd2de27ab815f57d17
LuigiEspinosa <numerocuatro@pm.me>
2023-08-23 01:10:57 -0500

    ci: Update .env File to use Vite

--- apps/tesla-clone/src/firebase/index.ts
+++ apps/tesla-clone/src/firebase/index.ts
@@ -1,10 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
-import { environment } from '../../environments/environment';
 
 // Your web app's Firebase configuration
-const firebaseConfig = environment.firebase;
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_TESLA_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_TESLA_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_TESLA_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_TESLA_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_TESLA_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_TESLA_APP_ID,
+};
 
 // Initialize Firebase
 const app: FirebaseApp = initializeApp(firebaseConfig);
